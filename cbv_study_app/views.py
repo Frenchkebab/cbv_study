8b727008779c0138ee26f7552ac2994950b7692c
Junghyun Choi <tubowangin@naver.com>
2019-02-18 16:37:43 +0900

    TemplateView로 CBV를 사용하는 경우

--- cbv_study_app/views.py
+++ cbv_study_app/views.py
@@ -3,3 +3,7 @@ from djang.views.generic import Templateview
 
 def murlsyview(request):
     return render(request, 'cbv_study_app/myview.html')
+
+
+myview = Template.as_view(
+    template_name = 'myapp/myview.html')
